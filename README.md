0293907f8d43ee9b9f5f2d42bf0eacd207b863b3
mrzhao <zhaomengru2018@gmail.com>
2023-02-28 18:19:16 +0800

    feat: using wee_alloc

--- README.md
+++ README.md
@@ -10,6 +10,10 @@ cargo install twiggy
 twiggy top target/wasm32-unknown-unknown/release/rust_example.wasm
 # use rust to add custom sections to a WebAssembly module
 ../wabt/build/wasm-objdump -s -j surmsection target/wasm32-unknown-unknown/release/rust_example.wasm
+#wasm-opt for WebAssembly VM instructions optimization (https://github.com/WebAssembly/binaryen)
+wasm-opt -O3 -o output.wasm target/wasm32-unknown-unknown/rust-example.wasm
+# no std support https://wasi.dev/
+# allocator https://crates.io/crates/wee_alloc
 ```
 
 ### Why code size is big
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,60 @@
-mod math {
-    mod math_js {
-        #[link(wasm_import_module = "Math")]
-        extern "C" {
-            pub fn random() -> f64;
-        }
-    }
-
-    pub fn random() -> f64 {
-        unsafe { math_js::random() }
-    }
-}
+// not to link against the standard library, only links against core.
+// 'core' makes no assumptions about the system the program will run on,
+// it provides APIs for language primitives, like float, strings,
+// APIs that expose processor features like atomic operations and SIMD instructions.
+// but lack of IO, heap memory allocations.
+#![no_std]
+// mod math {
+//     mod math_js {
+//         #[link(wasm_import_module = "Math")]
+//         extern "C" {
+//             pub fn random() -> f64;
+//         }
+//     }
+//
+//     pub fn random() -> f64 {
+//         unsafe { math_js::random() }
+//     }
+// }
+// One of the few occastions where we have to use `extern crate`
+// even in Rust Edition 2021.
+extern crate alloc;
+use alloc::vec::Vec;
 
 static PRIMES: &[i32] = &[2, 3, 5, 7, 11, 13, 17, 19, 23];
 
-const _: () = {
-    #[link_section = "surmsection"]
-    static SECTION_CONTENT: [u8; 11] = *b"hello world";
-};
-
-#[export_name = "add"]
-pub fn add(left: f64, right: f64) -> f64 {
-    left + right + unsafe { math::random() }
+#[panic_handler]
+fn panic(_panic: &core::panic::PanicInfo<'_>) -> ! {
+    loop {}
 }
 
+// const _: () = {
+//     #[link_section = "surmsection"]
+//     static SECTION_CONTENT: [u8; 11] = *b"hello world";
+// };
+
+// #[export_name = "add"]
+// pub fn add(left: f64, right: f64) -> f64 {
+//     left + right + unsafe { math::random() }
+// }
+
 #[no_mangle]
-extern "C" fn nth_prime(n: usize) -> i32{
+extern "C" fn nth_prime(n: usize) -> usize{
     // if n < 0 || n >= PRIMES.len() { return -1; }
     // PRIMES[n]
     // PRIMES.get(n).copied().unwrap_or(-1)
-    unsafe { *PRIMES.get_unchecked(n) }
+    // unsafe { *PRIMES.get_unchecked(n) }
+    // Please enjoy this horrible implementation of
+    // The Sieve of Eratosthenes.
+    let mut primes: Vec<usize> = Vec::new();
+    let mut current = 2;
+    while primes.len() < n {
+        if !primes.iter().any(|prime| current % prime == 0) {
+            primes.push(current);
+        }
+        current += 1;
+    }
+    primes.into_iter().last().unwrap_or(0)
 }
 
 #[cfg(test)]
